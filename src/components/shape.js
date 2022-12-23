5f1eaa83fc2354539942742b0299d7d72c016516
dipul9898 <118454254+dipul9898@users.noreply.github.com>
2022-12-23 23:36:52 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+    constructor(){
+        this.color = "green";
+    }
+    drawShape(){
+
+    }
+    calculateArea(){
+
+    }
+}
+module.exports = Shape;
